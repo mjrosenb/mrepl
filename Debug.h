88179a6fcd1d43f86fca267e049a8db09d196e38
marty rosenberg <marty.rosenberg@gmail.com>
2014-06-04 01:33:32 -0400

    Don't spew to stderr, spew to a file.

--- Debug.h
+++ Debug.h
@@ -1,8 +1,10 @@
 #ifndef __DEBUG_H
 #define __DEBUG_H
+#include <stdio.h>
 #if 0
 #define log(...)
 #else
-#define log(FMT, ...) do { fprintf(stderr, "%s:%d: " FMT, __FILE__, __LINE__, ##__VA_ARGS__); } while(0)
+extern FILE* errfd;
+#define log(FMT, ...) do { fprintf(errfd, "%s:%d: " FMT, __FILE__, __LINE__, ##__VA_ARGS__); } while(0)
 #endif
 #endif
--- MREPL.cc
+++ MREPL.cc
@@ -1,10 +1,13 @@
 
 #include "FrontEnd.h"
 /* for terminfo usage only. curses input/output is never used */
-
+FILE* errfd;
 
 int main()
 {
+    errfd = fopen("err.log", "w");
+    if (!errfd)
+        errfd = stderr;
     ui_init();
     ui_loop();
 }
